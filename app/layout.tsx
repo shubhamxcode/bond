531ad75973e37cd89600df4012e39f42f682530f
shubhamxcode <shubh.varshneycode@gmail.com>
2025-01-01 23:29:02 +0530

    improve file structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Home from './Home/intro/page';
 import Progressbar from '../components/ProgressBar';
 import Sessionwrap from '../app/camponent/sessionwraper';
 import Link from "next/link";
+import Nav from './nav/page'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,30 +30,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Sessionwrap> 
+      
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} > 
-          <Progressbar />
-          <div className="border border-gray-800 shadow-2xl py-2 hover:cursor-pointer sticky z-50 bg-slate-800 top-0">
-            <nav className="flex flex-col md:flex-row items-center justify-around">
-              <ul className="flex space-x-10">
-                <li>Product</li>
-                <li>Solution</li>
-                <li>Resources</li>
-                <li>Pricing</li>
-              </ul>
-              <ul className="flex items-center space-x-10">
-                <li>
-                  <Link href="/signin">Log in</Link>
-                </li>
-                <li>Contact</li>
-                <li className="bg-blue-700 p-2 rounded-xl">Get started - Free</li>
-              </ul>
-            </nav>
-          </div>
-          <Home />
-          {children}
+        <Nav/>
+        <Progressbar/>
+        <Home/>
         </body>
-      </Sessionwrap>
+      
     </html>
   );
 }
